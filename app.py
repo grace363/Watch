c9fec59de3ab069b5a44150f9584a062f62b6509
grace363 <gracewambuigw170@gmail.com>
2025-07-01 15:32:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,6 @@ serializer = URLSafeTimedSerializer(app.secret_key)
 
 class User(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False) 
     password_hash = db.Column(db.String(200), nullable=False) 
     account_type = db.Column(db.String(10), nullable=False) 
